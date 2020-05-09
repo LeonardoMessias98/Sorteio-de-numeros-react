d89c513a317c7c68b1bfa9ad2970471f990c9d03
Leonardo Messias <55189046+LeonardoMessias98@users.noreply.github.com>
2020-05-08 21:48:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [ valor, setValor ] = useState([]);
+
+  function calculate(){
+    const qtdImg = document.querySelector('input');
+
+    if(qtdImg.value == ''){
+      alert('CAMPO VAZIO');
+
+      return;
+    }
+
+    setValor([Math.floor(Math.random() * qtdImg.value + 1)]);
+  }
+
+  function clear(){
+    const qtdImg = document.querySelector('input');
+
+    qtdImg.value = ''
+
+    setValor('');
+  }
+
+  return (
+    <div className='center'>
+      <div className='mainText'>
+        <h1>Escolher fotos para o luan</h1>
+        <h2>Resultado: {valor}</h2>
+      </div>
+      <div className='inputs'>
+        <input className='inputContent' type='text' placeholder='Quantas imagens foram enviadas ?'/>
+
+        <div className='buttons' >
+          <button
+            onClick={calculate}
+            >Clique Para calcular</button>
+          <button 
+            onClick={clear}
+            >Limpar</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
